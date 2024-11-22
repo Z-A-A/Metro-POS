5603a06c26a3d5d1dbb5501036f5025604dab1af
akcena14 <qureshi.ahmad.karim@gmail.com>
2024-11-23 01:11:46 +0500

    updated schema

--- src/main/java/org/ZAA/backend/Controller/SuperAdminController.java
+++ src/main/java/org/ZAA/backend/Controller/SuperAdminController.java
@@ -1,2 +1,39 @@
-package org.ZAA.backend.Controller;public class SuperAdminController {
+package org.ZAA.backend.Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SuperAdminController {
+
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/your_database";
+    private static final String DB_USER = "your_username";
+    private static final String DB_PASSWORD = "your_password";
+
+    public SuperAdmin getSuperAdminById(int superAdminId) {
+        SuperAdmin superAdmin = null;
+        String query = "SELECT * FROM SuperAdmin WHERE SuperAdminID = ?";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
+            preparedStatement.setInt(1, superAdminId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                superAdmin = new SuperAdmin();
+                superAdmin.setSuperAdminID(resultSet.getInt("SuperAdminID"));
+                superAdmin.setName(resultSet.getString("Name"));
+                superAdmin.setEmail(resultSet.getString("Email"));
+                superAdmin.setPassword(resultSet.getString("Password"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return superAdmin;
+    }
 }
